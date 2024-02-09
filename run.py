504da1e4775d4734af28f7f23e0b566d0f664efa
alsona1188 <>
2024-02-09 23:07:44 +0000

    Creating clear function to make the comand-line interface more friendly. Creating the function for welcome message

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from userData import userData
+from rich import print
+import time
+import datetime
+import os
 
 # This code is taken from Love Sandwiches Project
 
@@ -17,3 +22,24 @@ SHEET = GSPREAD_CLIENT.open('atm_python')
 # user_data = SHEET.worksheet('data-user')
 # users = user_data.get_all_values()
 # print(users)
+
+def clear():
+    """clear function on windows or linux/mac"""
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def welcome_message():
+    clear()
+    now = datetime.datetime.now()
+    print(now.strftime("      [cyan]%A       %Y-%m-%d  %H:%M:%S[/]"))
+    time.sleep(0.2)
+    print("[green]==================================================[/]")
+    time.sleep(0.2)
+    print("      [cyan]Welcome to[/]   [bright_magenta]Commerz Bank[/]")
+    time.sleep(0.2)
+    print("[green]==================================================[/]")
+    time.sleep(0.2)
+
+welcome_message()

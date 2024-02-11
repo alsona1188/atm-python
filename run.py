592e0dc02bff8f2a581fb55d9d6237ab2193c419
alsona1188 <>
2024-02-11 03:29:07 +0000

    Finding where was the issue in the function of validate_card() and recreating it again

--- run.py
+++ run.py
@@ -20,8 +20,10 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('atm_python')
 
-user_data = SHEET.worksheet('data-user')
-users = user_data.get_all_values()
+#user_data = SHEET.worksheet('data-user')
+
+# Prints a list of users by row
+#list_of_users = user_data.get_all_values()[1:]
 # print(users)
 
 def clear():
@@ -62,60 +64,37 @@ def display_menu():
     print("[green]==================================================[/]")
     time.sleep(0.2)
 
-def validate_card(userData):
-    """ 
-    Checking if the card number belongs to the list of card numbers that we 
-    have on spreadsheet.
+
+def validate_card():
     """
-    try:
-        card_number = int(input("\n Please insert your card number:  "))
-        list_of_cardNumbers = user_data.col_values(3)
-        list_of_cardNumbers.pop(0)
-        list_of_cardNumbers = [int(x) for x in list_of_cardNumbers]
-        if card_number in list_of_cardNumbers:
-
-            print('\n      [green]Your card number is correct!!![/]')
-
-            return str(card_number)
-
-    except ValueError:
-        print('   [cyan]Please enter numbers for your account number![/]')
-    print("       [red]Your account number doesn't exist![/]")
-    return False
-
-def validate_pin(userData, card_number):
-    """" 
-    Verifying if the pin entered by the user is 
-    corresponding to our spreadsheet and also 
-    to the card number that the user entered
+    Validating the card inserted by the user. If it is correct will break
+    otherwise it will continue to ask for card number.
     """
-    card_number_row = user_data.find(card_number).row
-    card_number_col = user_data.find(card_number).col
-    user_pin = user_data.cell(card_number_row, card_number_col+1).value
-    
-    attempts_left = 3
-    while attempts_left > 0:
-        inserted_pin = int(input("\n Please insert your PIN:  "))
-
-        if user_pin == inserted_pin:
-            print("Your PIN is correct!")
-            return True
+    list_of_users = SHEET.worksheet("data-user").get_all_values()[1:]
+    while True:
+        inserted_card = input("\n Please insert your CARD:  ")
+        if not inserted_card:
+            print("\n Please insert your card number.")
+            continue
+            
+        user = [holder_card for holder_card in list_of_users if 
+                inserted_card == holder_card[2]]
+        if user:
+            time.sleep(0.3)
+            print("\n [green] Your card is correct[/]\n")
+            break
         else:
-            attempts_left -= 1
-            print(f"Incorrect PIN! {attempts_left} attempts left.")
-            if attempts_left == 0:
-                print("Your account has been blocked! Please contact us.")
-                return False
+            print("\n [red] Your card number doesn't exist![/]")
 
+def validate_pin(userData):
     
-    
+
+
+
 def main():
-
     welcome_message()
     display_menu()
-    validate_card(userData)
-   
+    validate_card()
     
 
-if __name__ == '__main__':
-    main()
+main()
--- userData.py
+++ userData.py
@@ -1,4 +1,4 @@
-class userData ():
+class userData():
     """
     userData class contains 5 attributes
     """
@@ -8,6 +8,41 @@ class userData ():
         self.card_number = card_number
         self.pin = pin
         self.balance = balance
+
+
+# Getter methods
+
+def get_firstname(self):
+    return self.firstname
+
+def get_lastname(self):
+    return self.lastname
+
+def get_card_number(self):
+    return self.card_number
+
+def get_pin(self):
+    return self.pin
+
+def get_balance(self):
+    return self.balance
+
+# Setter methods
+
+def set_firstname(self, newValue):
+    self.firstname = newValue
+
+def set_lastname(self, newValue):
+    self.lastname = newValue
+
+def set_card_number(self, newValue):
+    self.card_number = newValue
+
+def set_pin(self, newvalue):
+    self.pin = newvalue
+
+def set_balance(self, newValue):
+    self.balance = newValue
 
 
 def print_user_data(self):
